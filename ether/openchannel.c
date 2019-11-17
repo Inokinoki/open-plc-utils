1f3483812b5d88bff3fbfed2a5cdeafe36f4d610
Weixuan XIAO <veyx.shaw@gmail.com>
2019-11-17 17:05:15 +0100

    Add comment to learn more about sockaddr_ll

--- ether/openchannel.c
+++ ether/openchannel.c
@@ -98,6 +98,17 @@ signed openchannel (struct channel * channel)
 #if defined (__linux__)
 
 	struct ifreq ifreq;
+	/*
+	struct sockaddr_ll {
+		unsigned short sll_family;   // Always AF_PACKET
+		unsigned short sll_protocol; // Physical-layer protocol
+		int            sll_ifindex;  // Interface number
+		unsigned short sll_hatype;   // ARP hardware type
+		unsigned char  sll_pkttype;  // Packet type
+		unsigned char  sll_halen;    // Length of address
+		unsigned char  sll_addr[8];  // Physical-layer address
+	};
+	*/
 	struct sockaddr_ll sockaddr_ll =
 	{
 		PF_PACKET,
